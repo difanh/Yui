5172e4e03c3e0bc374d888c8179a64946f33228c
Diego Andrade <diegoandrade@cmu-365156.wv.cc.cmu.edu>
2012-07-24 10:43:33 -0400

    Initial Commit

---
+++ Yui/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Yui
+//
+//  Created by Diego Andrade on 7/24/12.
+//  Copyright (c) 2012 Carnegie Mellon University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
